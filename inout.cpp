0cb86d4bdeea4d0e1daaf4ffac8d1ed6fc49bb79
taaaepang <ggp02261@gmail.com>
2020-07-03 17:31:20 +0900

    c++ 입출력 관련.

---
+++ inout.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    // cin, cout은 printf, scanf보다 느린데 이 방법으로 빠르게 할 수 있다.
+}
